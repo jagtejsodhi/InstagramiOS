026240bfe47ee5f380b5a8fae0b999978ec7f638
jagtej <jagtej@yahoo-inc.com>
2015-09-14 12:48:30 -0700

    Initial Commit

---
+++ InstagramClient/JSViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSViewController.h
+//  InstagramClient
+//
+//  Created by Jagtej Sodhi on 9/14/15.
+//  Copyright (c) 2015 Jagtej Sodhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSViewController : UIViewController
+
+@end
---
+++ InstagramClient/JSViewController.m
@@ -0,0 +1,29 @@
+//
+//  JSViewController.m
+//  InstagramClient
+//
+//  Created by Jagtej Sodhi on 9/14/15.
+//  Copyright (c) 2015 Jagtej Sodhi. All rights reserved.
+//
+
+#import "JSViewController.h"
+
+@interface JSViewController ()
+
+@end
+
+@implementation JSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
